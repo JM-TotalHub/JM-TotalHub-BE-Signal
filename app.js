ff639b57207336a411419906172d5fb82de96455
jundev95 <junmop950dev@gmail.com>
2024-08-10 19:05:37 +0900

    로그 추가

--- app.js
+++ app.js
@@ -58,6 +58,13 @@ app.get('/api/test', (req, res) => {
   res.status(200).send({ message: 'API 연결 확인 완료' });
 });
 
+io.engine.on('connection_error', (err) => {
+  console.log(err.req); // the request object
+  console.log(err.code); // the error code, for example 1
+  console.log(err.message); // the error message, for example "Session ID unknown"
+  console.log(err.context); // some additional error context
+});
+
 const PORT = 7000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

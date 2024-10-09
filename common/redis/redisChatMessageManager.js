539abbcd92d7d8d6a4e1e9df6accd49a9284dc7e
jundev95 <junmop950dev@gmail.com>
2024-10-09 23:56:45 +0900

    채팅 메시지 저장 로직 구현

--- common/redis/redisChatMessageManager.js
+++ common/redis/redisChatMessageManager.js
@@ -1,71 +1,75 @@
-import { createClient } from 'redis';
-import ENV from '../utils/env';
 import api from '../connection/api';
+import RedisManager from './redisManager';
 
-const redisClient = createClient({
-  url: `redis://${ENV.SIGNAL_SERVER_REDIS_URL}:${ENV.SIGNAL_SERVER_REDIS_PORT}`,
-  password: ENV.SIGNAL_SERVER_REDIS_PASSWORD,
-});
+const redisClient = RedisManager.getClient();
 
-const connectClient = async () => {
-  try {
-    await redisClient.connect();
-    console.log(`Signal Redis Connected`);
-  } catch (err) {
-    console.error(`Error connecting to RedisClient:`, err);
-  }
-};
+const RedisChatMessageManager = (() => {
+  // 모든 활성화된 채팅방의 메시지를 가져오는 함수
+  const fetchAllChatRoomsMessages = async () => {
+    // 모든 채팅방 메시지 키 목록을 가져옵니다.
+    const chatRoomMessageKeys = await redisClient.keys('chat-room:*-messages');
+    const chatRoomsMessages = {};
 
-// 모든 활성화된 채팅방의 메시지를 가져오는 함수
-const fetchAllChatRoomsMessages = async () => {
-  // 모든 채팅방 메시지 키 목록을 가져옵니다.
-  const chatRoomMessageKeys = await redisClient.keys('chat-room:*-messages');
-  const chatRoomsMessages = {};
+    for (const key of chatRoomMessageKeys) {
+      // const chatRoomId = key.split(':')[1]; // 채팅방 ID 추출 (예: "1" 추출)
+      // console.log('key : ', key);
+      // console.log('chatRoomId : ', chatRoomId);
 
-  for (const key of chatRoomMessageKeys) {
-    const chatRoomId = key.split(':')[1]; // 채팅방 ID 추출 (예: "1" 추출)
-    const messages = await redisClient.lRange(key, 1, -1); // 0부터 -1까지 모든 메시지 가져오기
-    chatRoomsMessages[chatRoomId] = messages.map((message) =>
-      JSON.parse(message)
-    );
-  }
+      const match = key.match(/chat-room:(\d+)-messages/);
+      const chatRoomId = match ? match[1] : null;
+      console.log('chatRoomId : ', chatRoomId);
 
-  return chatRoomsMessages;
-};
-
-// 메시지를 API에 묶어서 저장하는 함수
-const saveMessages = async () => {
-  try {
-    const chatRoomsMessages = await fetchAllChatRoomsMessages();
-
-    for (const [chatRoomId, messages] of Object.entries(chatRoomsMessages)) {
-      if (messages.length > 0) {
-        await api.post('/messages', { chatRoomId, messages });
-        console.log(`Saved messages for chat room: ${chatRoomId}`);
-
-        // 성공적으로 저장한 메시지는 Redis에서 삭제
-        await redisClient.del(`chat-room:${chatRoomId}-messages`);
-      }
+      const messages = await redisClient.lRange(key, 0, -1); // 1부터 -1까지 모든 메시지 가져오기
+      chatRoomsMessages[chatRoomId] = messages.map((message) =>
+        JSON.parse(message)
+      );
     }
-  } catch (error) {
-    console.error('Error saving messages to API:', error);
-  }
-};
+    // 채팅방 ID를 키값으로 배열에 메시지 객체들 삽입
+    return chatRoomsMessages;
+  };
 
-// 주기적으로 API에 메시지를 저장하는 함수
-const startSavingMessages = (interval) => {
-  setInterval(saveMessages, interval);
-};
+  // 메시지를 API에 묶어서 저장하는 함수
+  const saveMessages = async () => {
+    try {
+      const chatRoomsMessages = await fetchAllChatRoomsMessages();
 
-// 연결 및 주기적 메시지 저장 시작
-const init = async () => {
-  await connectClient();
-  startSavingMessages(5000); // 5초마다 메시지 저장
-};
+      for (const [chatRoomId, messages] of Object.entries(chatRoomsMessages)) {
+        if (messages.length > 0) {
+          await api.post('/chats/chat-rooms/messages', {
+            chatRoomId,
+            messages,
+          });
+          console.log(`Saved messages for chat room: ${chatRoomId}`);
 
-const redisChatMessageManager = {
-  startSavingMessages,
-  init,
-};
+          // 성공적으로 저장한 메시지는 Redis에서 삭제
+          await redisClient.del(`chat-room:${chatRoomId}-messages`);
+        }
+      }
+    } catch (error) {
+      console.error('Error saving messages to API:', error.errorMessage);
+    }
+    console.log('메시지 저장 로직 동작완료');
+  };
 
-export default redisChatMessageManager;
+  const saveMessagesByChatRoomId = async (chatRoomId) => {
+    const key = `chat-room:${chatRoomId}-messages`;
+    const rawMessages = await redisClient.lRange(key, 0, -1);
+
+    const messages = rawMessages.map((message) => JSON.parse(message));
+
+    try {
+      if (messages.length > 0) {
+        await api.post('/chats/chat-rooms/messages', {
+          chatRoomId,
+          messages,
+        });
+      }
+      await redisClient.del(`chat-room:${chatRoomId}-messages`);
+    } catch (error) {
+      console.error('Error saving messages to API:', error.errorMessage);
+    }
+  };
+  return { saveMessages, saveMessagesByChatRoomId };
+})();
+
+export default RedisChatMessageManager;

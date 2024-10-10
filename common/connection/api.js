f34e6ad65aeb87a8e8576f96135f8ff79607dde3
jundev95 <junmop950dev@gmail.com>
2024-10-11 08:19:01 +0900

    로그 추가01

--- common/connection/api.js
+++ common/connection/api.js
@@ -2,7 +2,7 @@ import ENV from '../utils/env';
 import axios from 'axios';
 
 const api = (() => {
-  console.log('api 로그');
+  console.log('초기 api 로그');
   console.log('ENV.EXPRESS_SERVER_BASE_URL : ', ENV.EXPRESS_SERVER_BASE_URL);
   console.log(
     'ENV.CORS_REACT_SERVER_BASE_URL : ',
@@ -19,6 +19,20 @@ const api = (() => {
     backendApi.interceptors.request.use(
       (config) => {
         console.log('정상 요청');
+        // 요청마다 환경 변수를 출력
+        console.log('API 호출 시 환경변수 로그:');
+        console.log(
+          'ENV.EXPRESS_SERVER_BASE_URL : ',
+          ENV.EXPRESS_SERVER_BASE_URL
+        );
+        console.log(
+          'ENV.CORS_REACT_SERVER_BASE_URL : ',
+          ENV.CORS_REACT_SERVER_BASE_URL
+        );
+        console.log(
+          'ENV.SIGNAL_SERVER_REDIS_URL : ',
+          ENV.SIGNAL_SERVER_REDIS_URL
+        );
         return config;
       },
       (error) => {
